c4cad4f3159b99b716f83c306748ffe8ea3560b2
Elmer van Chastelet <evanchastelet@gmail.com>
2013-03-04 14:43:30 +0100

    Fix construction of Entry objs using createNew()

--- nativejava/org/webdsl/reposearch/repositories/RepositoryFetcher.java
+++ nativejava/org/webdsl/reposearch/repositories/RepositoryFetcher.java
@@ -319,7 +319,7 @@ public class RepositoryFetcher {
           }
         } else {
           boolean isBinFile = true;
-          Entry c = new Entry();
+          Entry c = Entry.createEmpty();
           c.setNameNoEventsOrValidation ( f.getName() );
           c.setUrlNoEventsOrValidation ( fixUrl ( repo+dir+f.getName() ) );
           if ( !hasBinaryFileExtension ( f.getName() ) ) {
@@ -372,7 +372,7 @@ public class RepositoryFetcher {
     String fileName = url.substring ( url.lastIndexOf ( '/' ) +1 );
     String content = null, contentFixed = null;
     boolean isBinFile = true;
-    Entry c = new Entry();
+    Entry c = Entry.createEmpty();
     c.setNameNoEventsOrValidation ( fileName );
     c.setUrlNoEventsOrValidation ( fixUrl ( url ) );
     if ( ! hasBinaryFileExtension ( fileName ) ) {
